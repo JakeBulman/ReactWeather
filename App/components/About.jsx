b98672ff48032b1b500f244295207f51b2984371
Jake Bulman <jakebulman@hotmail.com>
2017-09-14 19:36:53 +0100

    Style about page and link to resources

--- App/components/About.jsx
+++ App/components/About.jsx
@@ -3,8 +3,21 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page.</p>
+      <h1 className="text-center">About</h1>
+      <p>Welcome to the About page. This app links you to the weather in the town you search for.</p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.githup.io/react">React</a> -
+          This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweatherapp.org">Open Weather App</a> -
+          I used Open Weather App to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   );
 }
